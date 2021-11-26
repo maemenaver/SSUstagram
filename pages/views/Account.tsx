eecd9ef9d614d2f9b59a96923c08214d07eb13e6
maemenaver <maemenaver@soongsil.ac.kr>
2021-11-26 14:45:32 +0900

    💄 Update to make formik for Account

--- pages/views/Account.tsx
+++ pages/views/Account.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@mui/material";
+import { Field, Form, Formik } from "formik";
+import { TextField } from "formik-mui";
 import { NextPageContext } from "next";
 import Head from "next/head";
 
@@ -15,30 +18,54 @@ export default function Account(props: AccountProps) {
                 <title>{title}</title>
             </Head>
             <div>
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="이메일 주소"
-                />
-                <br />
-                <input type="text" name="name" id="name" placeholder="성명" />
-                <br />
-                <input
-                    type="text"
-                    name="accountID"
-                    id="accountID"
-                    placeholder="사용자 이름"
-                />
-                <br />
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="비밀번호"
-                />
-                <br />
-                <button>가입</button>
+                <Formik
+                    initialValues={{
+                        email: "",
+                        password: "",
+                    }}
+                    onSubmit={(value, helper) => {}}
+                >
+                    {({ submitForm, isSubmitting }) => (
+                        <Form>
+                            <Field
+                                component={TextField}
+                                name="email"
+                                type="email"
+                                placeholder="이메일 주소"
+                            />
+                            <br />
+                            <Field
+                                component={TextField}
+                                name="name"
+                                type="text"
+                                placeholder="성명"
+                            />
+                            <br />
+                            <Field
+                                component={TextField}
+                                name="accountID"
+                                type="text"
+                                placeholder="사용자 이름"
+                            />
+                            <br />
+                            <Field
+                                component={TextField}
+                                name="password"
+                                type="password"
+                                placeholder="비밀번호"
+                            />
+                            <br />
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                disabled={isSubmitting}
+                                onClick={submitForm}
+                            >
+                                가입
+                            </Button>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </>
     );
