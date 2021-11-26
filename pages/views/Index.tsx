c78dcf04b6d773dfbd5425664118fdf2fe4d02b7
maemenaver <maemenaver@soongsil.ac.kr>
2021-11-26 14:35:03 +0900

    💄 Update to make formik for Index

--- pages/views/Index.tsx
+++ pages/views/Index.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@mui/material";
+import { Field, Form, Formik } from "formik";
+import { TextField } from "formik-mui";
 import { NextPageContext } from "next";
 import Head from "next/head";
 import Link from "next/link";
@@ -16,21 +19,40 @@ export default function Index(props: IndexProps) {
                 <title>{title}</title>
             </Head>
             <div>
-                <input
-                    type="text"
-                    name="accountID"
-                    id="accountID"
-                    placeholder="이메일 주소 또는 사용자 이름"
-                />
-                <br />
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="비밀번호"
-                />
-                <br />
-                <Link href="">로그인</Link>
+                <Formik
+                    initialValues={{
+                        email: "",
+                        password: "",
+                    }}
+                    onSubmit={(value, helper) => {}}
+                >
+                    {({ submitForm, isSubmitting }) => (
+                        <Form>
+                            <Field
+                                component={TextField}
+                                name="accountID"
+                                type="accountID"
+                                placeholder="이메일 주소 또는 사용자 이름"
+                            />
+                            <br />
+                            <Field
+                                component={TextField}
+                                name="password"
+                                type="password"
+                                placeholder="비밀번호"
+                            />
+                            <br />
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                disabled={isSubmitting}
+                                onClick={submitForm}
+                            >
+                                로그인
+                            </Button>
+                        </Form>
+                    )}
+                </Formik>
             </div>
             <div>
                 계정이 없으신가요? <Link href="/account">회원가입</Link>
