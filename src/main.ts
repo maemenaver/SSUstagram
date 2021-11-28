2c7e4e3d4c5586f31bcddf5250b76b031709719c
maemenaver <maemenaver@soongsil.ac.kr>
2021-11-29 02:07:58 +0900

    🔧 Use bodyParser, transform option

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        bodyParser: true,
+    });
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+        })
+    );
     await app.listen(8053);
 }
 bootstrap();
