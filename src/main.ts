23dce06f8461a8baa0acfb22af1330108a75e330
maemenaver <maemenaver@soongsil.ac.kr>
2021-11-30 23:55:18 +0900

    🌱 Use cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import cookieParser from "cookie-parser";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         bodyParser: true,
     });
+    app.use(cookieParser());
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true,
