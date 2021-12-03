81852a03079d22f4634b1fe75a00a91da85829f5
maemenaver <maemenaver@soongsil.ac.kr>
2021-12-03 20:27:13 +0900

    🚩 Remove todo flag for token

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -4,11 +4,7 @@ export function getToken(req: Request) {
     return req.cookies["access_token"];
 }
 
-/*
- * 여기 나중에 고쳐야함
- */
 export function getBearerToken(req: Request) {
     const result = "Bearer " + getToken(req);
-    console.log("getBearerToken", result);
     return result;
 }
