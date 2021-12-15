5f45a2867f750ae4ede4bf8f7484d62625ccb40e
Jeonghun Ha <maemenaver@soongsil.ac.kr>
2021-12-15 10:46:31 +0900

    Use swcMinify

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/**
+ * https://nextjs.org/docs/api-reference/next.config.js/introduction
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    pageExtensions: ['tsx'],
+    swcMinify: true
+}
+
+module.exports = nextConfig
